0891fc02841fe01f57f0333f2cd3f4d8646d6f15
rindumasismara <ismara13rip@gmail.com>
2025-12-24 15:10:55 +0700

    Fix modelling by dropping categorical feature

--- Membangun_model/modelling.py
+++ Membangun_model/modelling.py
@@ -1,23 +1,39 @@
+import pandas as pd
 import mlflow
 import mlflow.sklearn
+
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, f1_score
-import pandas as pd
 
-mlflow.sklearn.autolog()
+mlflow.set_tracking_uri("file:./mlruns")
+mlflow.set_experiment("Fraud Detection Basic")
 
-df = pd.read_csv("onlinepaymentfraud_preprocessing.csv")
+def main():
+    df = pd.read_csv("onlinepaymentfraud_preprocessing.csv")
 
-X = df.drop(columns=["isFraud", "isFlaggedFraud"])
-y = df["isFraud"]
+    if "amount_bin" in df.columns:
+        df = df.drop(columns=["amount_bin"])
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, stratify=y, random_state=42
-)
+    X = df.drop(columns=["isFraud"])
+    y = df["isFraud"]
 
-with mlflow.start_run():
-    model = RandomForestClassifier(random_state=42)
-    model.fit(X_train, y_train)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
 
-    y_pred = model.predict(X_test)
+    with mlflow.start_run():
+        mlflow.sklearn.autolog()
+
+        model = RandomForestClassifier(random_state=42)
+        model.fit(X_train, y_train)
+
+        y_pred = model.predict(X_test)
+        acc = accuracy_score(y_test, y_pred)
+        f1 = f1_score(y_test, y_pred)
+
+        print("Accuracy:", acc)
+        print("F1 Score:", f1)
+
+if __name__ == "__main__":
+    main()
